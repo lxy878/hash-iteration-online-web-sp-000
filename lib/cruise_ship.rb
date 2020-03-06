06c89c04d98a8079ce48b0d65084ca557cfd48ce
Xiaoyi Liu <lxy878@gmail.com>
2020-03-06 13:21:18 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -9,7 +9,7 @@ require 'pry'
 def select_winner(passengers)
   # add the code snippet here!
   passengers.each do |key, name|
-    if key == "suite_a" and name.start_with?("A")
+    if key == :suite_a and name.start_with?("A")
       # binding.pry
       return name
     end
