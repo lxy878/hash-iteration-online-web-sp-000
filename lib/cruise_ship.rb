325063a9cd6a285cf2305e7781afa517a8050484
Xiaoyi Liu <lxy878@gmail.com>
2020-03-06 13:17:32 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -5,11 +5,12 @@
 # suite_d: "Charlie Chaplin",
 # suite_e: "Crumpet the Elf"
 # }
-
+require 'pry'
 def select_winner(passengers)
   # add the code snippet here!
   passengers.each do |key, name|
     if key == "suite_a" and name.start_with?("A")
+      binding.pry
       return name
     end
   end
